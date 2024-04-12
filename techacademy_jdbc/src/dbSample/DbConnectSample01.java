975b36122d0d04c3b3b79d355886d4c0ee26ed08
dir-FY24kajiwarashimpei <shinpei.kajiwara.h902@dirbato.co.jp>
2024-04-12 10:24:43 +0900

    SELECT文の実行を追加

--- techacademy_jdbc/src/dbSample/DbConnectSample01.java
+++ techacademy_jdbc/src/dbSample/DbConnectSample01.java
@@ -1,6 +1,8 @@
 package dbSample;
 
 import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.ResultSet;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
@@ -8,18 +10,25 @@ public class DbConnectSample01 {
 
     public static void main(String[] args) {
         Connection con = null;
+        Statement stmt = null;
+        ResultSet rs = null;
 
         try {
             // 1. ドライバーのクラスをJava上で読み込む
             Class.forName("com.mysql.cj.jdbc.Driver");
 
             // 2. DBと接続する
-            Connection con = DriverManager.getConnection(
-                    "jdbc:mysql://localhost/world?useSSL=false&allowPublicKeyRetrieval=true", "root", "_Sakurai0125");
+            con = DriverManager.getConnection(
+                    "jdbc:mysql://localhost/world?useSSL=false&allowPublicKeyRetrieval=true",
+                    "root", 
+                    "_Sakurai0125");
 
             // 3. DBとやり取りする窓口(Statementオブジェクト)の作成
+            stmt = con.createStatement();
 
             // 4, 5. Select分の実行と結果を格納/代入
+            String sql = "SELECT * FROM country LIMIT 50";
+            rs = stmt.executeQuery(sql);
 
             // 6. 結果を表示する
 
@@ -31,6 +40,23 @@ public class DbConnectSample01 {
             e.printStackTrace();
         } finally {
             // 7. 接続を閉じる
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException e) {
+                    System.err.println("ResultSetを閉じるときにエラーが発生しました。");
+                    e.printStackTrace();
+                }
+            }
+            if (stmt!= null) {
+                try {
+                    stmt.close();
+                } catch (SQLException e) {
+                    System.err.println("Statementを閉じるときにエラーが発生しました。");
+                    e.printStackTrace();
+                }
+            }
+            
             if (con != null) {
                 try {
                     con.close();
@@ -38,9 +64,9 @@ public class DbConnectSample01 {
                     System.err.println("データベース切断時にエラーが発生しました。");
                     e.printStackTrace();
                 }
-        
-        
             }
+        } 
+            
         }
 
     }
