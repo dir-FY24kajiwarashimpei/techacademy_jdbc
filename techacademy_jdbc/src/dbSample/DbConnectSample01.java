a23cbabd25e5ac1c83f795bea081ab80a194abe3
dir-FY24kajiwarashimpei <shinpei.kajiwara.h902@dirbato.co.jp>
2024-04-12 09:35:02 +0900

    DBConnection

--- techacademy_jdbc/src/dbSample/DbConnectSample01.java
+++ techacademy_jdbc/src/dbSample/DbConnectSample01.java
@@ -7,34 +7,41 @@ import java.sql.SQLException;
 public class DbConnectSample01 {
 
     public static void main(String[] args) {
-        
+        Connection con = null;
+
         try {
-            //1. ドライバーのクラスをJava上で読み込む
+            // 1. ドライバーのクラスをJava上で読み込む
             Class.forName("com.mysql.cj.jdbc.Driver");
-            
-            //2. DBと接続する
+
+            // 2. DBと接続する
             Connection con = DriverManager.getConnection(
-                    "jdbc:mysql://localhost/world?useSSL=false&allowPublicKeyRetrieval=true",
-                    "root",
-                    "_Sakurai0125"
-                    );
-            
-            //3. DBとやり取りする窓口(Statementオブジェクト)の作成
-            
-            //4, 5. Select分の実行と結果を格納/代入
-            
-            //6. 結果を表示する
-            
-            
+                    "jdbc:mysql://localhost/world?useSSL=false&allowPublicKeyRetrieval=true", "root", "_Sakurai0125");
+
+            // 3. DBとやり取りする窓口(Statementオブジェクト)の作成
+
+            // 4, 5. Select分の実行と結果を格納/代入
+
+            // 6. 結果を表示する
+
         } catch (ClassNotFoundException e) {
             System.err.println("JDBSドライバーのロードに失敗しました。");
             e.printStackTrace();
         } catch (SQLException e) {
             System.err.println("データベースに異常が発生しました。");
             e.printStackTrace();
-        }
+        } finally {
+            // 7. 接続を閉じる
+            if (con != null) {
+                try {
+                    con.close();
+                } catch (SQLException e) {
+                    System.err.println("データベース切断時にエラーが発生しました。");
+                    e.printStackTrace();
+                }
         
-            //7. 接続を閉じる
+        
+            }
+        }
 
     }
 
